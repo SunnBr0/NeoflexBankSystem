9ee9c8a041166d2640a0b66ca0217e1028b1c41a
Vladimir <vova.voronov.2002@bk.ru>
2025-06-10 17:13:44 +0300

    new currency card

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -3,12 +3,14 @@ import '../../style/homePage/header.css';
 import '../../style/homePage/main.css';
 import '../../style/homePage/credit-card.css';
 import '../../style/homePage/feature-info.css';
+import '../../style/homePage/currency-card.css';
 import card1 from '../../assets/credit-card/card1.svg';
 import card2 from '../../assets/credit-card/card2.svg';
 import card3 from '../../assets/credit-card/card3.svg';
 import card4 from '../../assets/credit-card/card4.svg';
 import feature from '../../assets/feature.svg';
 import icon from '../../assets/icon.svg';
+import iconBank from '../../assets/currency-card/icon-bank.svg';
 export const HomePage: React.FC = () => {
   return (
     <>
@@ -52,35 +54,83 @@ export const HomePage: React.FC = () => {
             <img src={feature} alt="" />
           </figure>
           <section className="feature-info__section">
-            <h2 className='feature-info__header'>We Provide Many Features You Can Use</h2>
-            <p>You can explore the features that we provide with fun and have their own functions each feature</p>
-            <ul className='feature-info__list'>
-              <li className='feature-info__item'>
-                <figure className='feature-info__icon feature-info__icon-1'>
+            <h2 className="feature-info__header">
+              We Provide Many Features You Can Use
+            </h2>
+            <p>
+              You can explore the features that we provide with fun and have
+              their own functions each feature
+            </p>
+            <ul className="feature-info__list">
+              <li className="feature-info__item">
+                <figure className="feature-info__icon feature-info__icon-1">
                   <img src={icon} alt="" />
                 </figure>
                 <p>Powerfull online protection.</p>
               </li>
-              <li className='feature-info__item'>
-                <figure className='feature-info__icon feature-info__icon-2'>
+              <li className="feature-info__item">
+                <figure className="feature-info__icon feature-info__icon-2">
                   <img src={icon} alt="" />
                 </figure>
                 <p>Cashback without borders.</p>
               </li>
-              <li className='feature-info__item'>
-                <figure className='feature-info__icon feature-info__icon-3'>
+              <li className="feature-info__item">
+                <figure className="feature-info__icon feature-info__icon-3">
                   <img src={icon} alt="" />
                 </figure>
                 <p>Personal design</p>
               </li>
-              <li className='feature-info__item'>
-                <figure className='feature-info__icon feature-info__icon-4'>
+              <li className="feature-info__item">
+                <figure className="feature-info__icon feature-info__icon-4">
                   <img src={icon} alt="" />
                 </figure>
                 <p>Work anywhere in the world</p>
               </li>
             </ul>
           </section>
+        </section>
+        <section className="currency-card">
+          <section className="currency-card__section">
+            <h2 className="currency-card__header">
+              Exchange rate in internet bank
+            </h2>
+            <p className="currency-card__title">Currency</p>
+            <ul className="currency-card__list">
+              <li className="currency-card__item">
+                <h3>USD:</h3>
+                <p>60.78</p>
+              </li>
+              <li className="currency-card__item">
+                <h3>CNY:</h3>
+                <p>9.08</p>
+              </li>
+              <li className="currency-card__item">
+                <h3>CHF:</h3>
+                <p>64.78</p>
+              </li>
+              <li className="currency-card__item">
+                <h3>USD:</h3>
+                <p>60.78</p>
+              </li>
+              <li className="currency-card__item">
+                <h3>JPY:</h3>
+                <p>0.46</p>
+              </li>
+              <li className="currency-card__item">
+                <h3>TRY:</h3>
+                <p>3.39</p>
+              </li>
+            </ul>
+            <p className="currency-card__subtext">All courses</p>
+          </section>
+          <aside className="currency-card__aside">
+            <p>
+              Update every 15 minutes, MSC 09.08.2022
+            </p>
+            <figure className="currency-card__figure">
+              <img src={iconBank} alt="" />
+            </figure>
+          </aside>
         </section>
       </main>
     </>
