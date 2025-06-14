dd445a7f6a21cab9b28317ba980c72eddcbea5cf
Vladimir <vova.voronov.2002@bk.ru>
2025-06-14 14:06:07 +0300

    apaptiv all except footer

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -14,10 +14,10 @@ import card4 from '../../assets/credit-card/card4.svg';
 import feature from '../../assets/feature.svg';
 import icon from '../../assets/icon.svg';
 import iconBank from '../../assets/currency-card/icon-bank.svg';
-// import map from '../../assets/map.svg';
-// import email from '../../assets/support/email.svg';
-// import send from '../../assets/support/send.svg';
-// import logo from '../../assets/logo.svg';
+import map from '../../assets/map.svg';
+import email from '../../assets/support/email.svg';
+import send from '../../assets/support/send.svg';
+import logo from '../../assets/logo.svg';
 export const HomePage: React.FC = () => {
   return (
     <>
@@ -137,7 +137,7 @@ export const HomePage: React.FC = () => {
             </figure>
           </aside>
         </section>
-        {/* <section className="map">
+        <section className="map">
           <h2 className="map__header">
             You can use our services anywhere in the world
           </h2>
@@ -170,9 +170,9 @@ export const HomePage: React.FC = () => {
               </button>
             </form>
           </article>
-        </section> */}
+        </section>
       </main>
-      {/* <footer className="footer">
+      <footer className="footer">
         <section className="footer__section">
           <section className="footer__section-header">
             <figure className="footer__logo">
@@ -203,7 +203,7 @@ export const HomePage: React.FC = () => {
             to use cookies, please change your browser settings
           </p>
         </section>
-      </footer> */}
+      </footer>
     </>
   );
 };
