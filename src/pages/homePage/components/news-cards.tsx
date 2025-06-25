4018a99823c81ef825a8e49299e6710af816fc4f
Vladimir <vova.voronov.2002@bk.ru>
2025-06-25 16:24:01 +0300

    the news block is ready

--- src/pages/homePage/components/news-cards.tsx
+++ src/pages/homePage/components/news-cards.tsx
@@ -1,9 +1,17 @@
-import "../../../style/homePage/news-cards/cards.scss"
-import "../../../style/homePage/news-cards/container-card.scss"
-import "../../../style/homePage/news-cards/slider.scss"
+import '../../../style/homePage/news-cards/cards.scss';
+import '../../../style/homePage/news-cards/container-card.scss';
+import '../../../style/homePage/news-cards/slider.scss';
+import '../../../style/homePage/news-cards/news-cards.scss';
 export const NewsCards = () => {
   return (
-    <>
+    <section className="news-cards">
+      <h2 className="news-cards__header">
+        Current news from the world of finance
+      </h2>
+      <p className="news-cards__title">
+        We update the news feed every 15 minutes. You can learn more by clicking
+        on the news you are interested in.
+      </p>
       <section className="container-card">
         <article className="card">
           <div className="card__content">
@@ -60,12 +68,36 @@ export const NewsCards = () => {
           </div>
         </article>
       </section>
-      <section id="slider">
-        <button id="prevBtn" disabled>
-          Назад
+      <section className="slider" id="slider">
+        <button className="slider__button" id="prevBtn" disabled>
+          <figure className="slider__figure">
+            <svg
+              width="28"
+              stroke="#222222"
+              height="28"
+              viewBox="0 0 25 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M25 17H9.84211V24.3914C9.84211 24.5845 9.59562 24.6655 9.48109 24.5101L1 13L9.48109 1.48994C9.59562 1.33452 9.84211 1.41552 9.84211 1.60858V9H25" />
+            </svg>
+          </figure>
         </button>
-        <button id="nextBtn">Вперед</button>
+        <button className="slider__button" id="nextBtn">
+          <figure className="slider__figure">
+            <svg
+              stroke="white"
+              width="28"
+              height="28"
+              viewBox="0 0 25 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 9H15.1579V1.60858C15.1579 1.41552 15.4044 1.33452 15.5189 1.48994L24 13L15.5189 24.5101C15.4044 24.6655 15.1579 24.5845 15.1579 24.3914V17H0" />
+            </svg>
+          </figure>
+        </button>
       </section>
-    </>
+    </section>
   );
 };
